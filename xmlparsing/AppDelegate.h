87ff906ee101f202c705cde8830a1bc11c3e07e6
Student <Student@apples-MacBook-Air-3.local>
2016-11-04 10:13:49 +0530

    Initial Commit

---
+++ xmlparsing/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  xmlparsing
+//
+//  Created by Student on 04/11/16.
+//  Copyright (c) 2016 Abhijit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ xmlparsing/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  xmlparsing
+//
+//  Created by Student on 04/11/16.
+//  Copyright (c) 2016 Abhijit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
